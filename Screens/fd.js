223626ff748797314af39097582723173240082b
AabhasAgrawal <73350120+AabhasAgrawal@users.noreply.github.com>
2021-07-22 08:44:22 +0530

    Add files via upload

---
+++ Screens/fd.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View,TextInput } from 'react-native';
+
+export default class Facebook extends React.Component {
+    render (){
+    return (
+      <View >
+          <Text>Here you may use facebook </Text>
+        
+      </View>
+    );
+    }
+  }
---
+++ Screens/in.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View,TextInput } from 'react-native';
+
+export default class Instagram extends React.Component {
+    render (){
+    return (
+      <View >
+          <Text>Here you may see Instagram</Text>
+      
+      </View>
+    );
+    }
+  }
